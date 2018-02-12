29995251955131ac813d9ad02e162438398837df
Ai Nguyen <ainguyenkaka@gmail.com>
2018-02-12 15:34:48 +0800

    add quick choose for labels and objects

--- js/pages/medias/image-gallery.js
+++ js/pages/medias/image-gallery.js
@@ -3,4 +3,8 @@ $(function () {
         thumbnail: true,
         selector: 'a'
     });
+    $('#aniimated-thumbnials-gallery').lightGallery({
+        thumbnail: true,
+        selector: 'a'
+    });
 });
